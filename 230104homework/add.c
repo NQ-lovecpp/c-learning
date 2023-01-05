644c63edf90b8ca9859d7f376c4d23a54e0c55ab
chenzhuowen4384 <2589149154@qq.com>
2023-01-05 08:39:43 +0800

    task

---
+++ 230104homework/add.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//	int a, b;
+//	scanf("%d %d", &a, &b);
+//	if (a > b)
+//		printf("\n%d", a);
+//	else if (a < b)
+//		printf("\n%d", b);
+//	else if (a == b)
+//		printf("a=b=%d", a);
+//	return 0;
+//
+//}
+
+#include <stdio.h>
+
+int main()
+{
+    double h1 = 0.00f;
+    scanf("%f", &h1);
+    printf("%f", h1);
+    return 0;
+}
+
+
+
+//
+//float salary = 10000.0f;
+//printf("%.5f\n", salary);
