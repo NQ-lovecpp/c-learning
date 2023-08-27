8cf5428f2c6f4cfde0da2114b3ba01ae29aea6ac
chenzhuowen4384 <2589149154@qq.com>
2023-08-27 19:39:33 +0800

    task

--- Vector_recite_1/vector.h
+++ Vector_recite_1/vector.h
@@ -14,7 +14,7 @@ namespace chen
 		}
 		iterator end()
 		{
-			return _end + 1;
+			return _finish + 1;
 		}
 		const_iterator cbegin() const
 		{
@@ -22,17 +22,55 @@ namespace chen
 		}
 		const_iterator cend() const
 		{
-			return _end + 1;
+			return _finish + 1;
 		}
 
 		// capacity
 		size_t size()
 		{
-
+			_finish - _start + 1;
 		}
-		size_t capacity() const;
-		void reserve(size_t n);
-		void resize(size_t n, const T& value = T());
+
+		size_t capacity() const
+		{
+			_endOfStorage - _start + 1;
+		}
+
+		void reserve(size_t n)
+		{
+			if (n > _capacity())
+			{
+				T* tmp = new T[n];
+				size_t sz = size();
+				if (_start)
+				{
+					memcpy(tmp, _start, sizeof(T) * sz);
+				}
+			}
+			_start = tmp;
+			_finish = _start + sz - 1;
+			_endOfStorage = _start + n;
+		}
+
+		void resize(size_t n, const T& value = T())// 这里的“=”是初始化常量引用
+		{
+			if (n <= capacity())
+			{
+				_finish = _start + n - 1;
+			}
+			else
+			{
+				reserve(n);
+				while (_finish < _start + n)
+				{
+					*_finish = value;
+					// 调用赋值运算符重载，因为_finish所指向的对象是new出来的，
+					//new已经调用了构造函数，可以说finish所指向的对象已经被定义，
+					//对于已经定义的对象，当然=号调用的是赋值运算符重载
+					++_finish;
+				}
+			}
+		}
 
 		// access
 		T& operator[](size_t pos);
